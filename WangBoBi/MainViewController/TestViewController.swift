e21e5fd4184a9b6310a88b1d95441ed6d2420b21
郑东喜 <zhengdongxi@zhengdongxideMacBook-Pro.local>
2017-06-18 11:54:28 +0800

    有问题的第三方介入输出。

---
+++ WangBoBi/MainViewController/TestViewController.swift
@@ -0,0 +1,50 @@
+//
+//  TestViewController.swift
+//  WangBoBi
+//
+//  Created by 郑东喜 on 2017/6/18.
+//  Copyright © 2017年 郑东喜. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    lazy var poassV: XLPasswordInputView = {
+        
+        let d : XLPasswordInputView = XLPasswordInputView.init(frame: CGRect.init(x: 0, y: 0, width: SCREEN_WIDTH, height: 100))
+        XLPasswordInputView.init(passwordLength: 5)
+        
+        d.passwordBlock = {(params) -> Void in
+            CCog(message: params as! String)
+        }
+        return d
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.addSubview(poassV)
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
