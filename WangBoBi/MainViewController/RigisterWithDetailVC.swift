5c69cc24e02b6a582401aae8aeb865a8a4360043
郑东喜 <zhengdongxi@zhengdongxideMacBook-Pro.local>
2017-06-19 20:31:41 +0800

    commit

--- WangBoBi/MainViewController/RigisterWithDetailVC.swift
+++ WangBoBi/MainViewController/RigisterWithDetailVC.swift
@@ -19,28 +19,28 @@ class RigisterWithDetailVC: UIViewController {
     
     
     // MARK: - 登录
-    lazy var loginBtn: UIButton = {
+    fileprivate lazy var loginBtn: UIButton = {
         let d : UIButton = UIButton.init(frame: CGRect.init(x: SCREEN_WIDTH * 0.545088542256378, y: SCREEN_HEIGHT * 0.916213761205259, width: 50 * SCREEN_SCALE, height: 30 * SCREEN_SCALE))
         d.addTarget(self, action: #selector(jumpTologin), for: .touchUpInside)
         return d
     }()
     
     // MARK: - 邀请码
-    lazy var yaoqingBtn: UIButton = {
+    fileprivate lazy var yaoqingBtn: UIButton = {
         let d: UIButton = UIButton.init(frame: CGRect.init(x: SCREEN_WIDTH * 0.18438001992046, y: SCREEN_HEIGHT * 0.407608695652174, width: SCREEN_WIDTH * 0.88 - SCREEN_WIDTH * 0.213, height: 30 * SCREEN_SCALE))
         d.layer.borderWidth = 1
         return d
     }()
     
     // MARK: - 登录密码
-    lazy var loginPass: UIButton = {
+    fileprivate lazy var loginPass: UIButton = {
         let d : UIButton = UIButton.init(frame: CGRect.init(x: SCREEN_WIDTH * 0.18438001992046, y: SCREEN_HEIGHT * 0.504076086956522, width: SCREEN_WIDTH * 0.88 - SCREEN_WIDTH * 0.213, height: 30 * SCREEN_SCALE))
         d.layer.borderWidth = 1
         return d
     }()
     
     // MARK: - 登录密码
-    lazy var repeatPass: UIButton = {
+    fileprivate lazy var repeatPass: UIButton = {
         let d :UIButton = UIButton.init(frame: CGRect.init(x: SCREEN_WIDTH * 0.18438001992046, y: SCREEN_HEIGHT * 0.60054347826087, width: SCREEN_WIDTH * 0.88 - SCREEN_WIDTH * 0.213, height: 30 * SCREEN_SCALE))
         d.layer.borderWidth = 1
         return d
@@ -48,20 +48,22 @@ class RigisterWithDetailVC: UIViewController {
     
     
     // MARK: - 用户协议
-    lazy var userAgree: UIButton = {
+    fileprivate lazy var userAgree: UIButton = {
         let d : UIButton = UIButton.init(frame: CGRect.init(x: SCREEN_WIDTH * 0.355877604461523, y: SCREEN_HEIGHT * 0.665307957193126, width: 100 * SCREEN_SCALE, height: 30 * SCREEN_SCALE))
         d.layer.borderWidth = 1
         return d
     }()
     
     // MARK: - 协议图标
-    lazy var agreeeBtn: UIButton = {
+    fileprivate lazy var agreeeBtn: UIButton = {
         let d : UIButton = UIButton.init(frame: CGRect.init(x: 0.0901771213697351 * SCREEN_WIDTH, y: SCREEN_HEIGHT * 0.667119565217391, width: 12 * SCREEN_SCALE, height: 12 * SCREEN_SCALE))
         d.contentMode = UIViewContentMode.scaleAspectFit
         d.addTarget(self, action: #selector(agreeSEL(sender:)), for: .touchUpInside)
         return d
     }()
     
+    
+    // MARK: - 同意协议按钮
     lazy var agreeeImg: UIImageView = {
         let d : UIImageView = UIImageView.init(frame: CGRect.init(x: 0.0901771213697351 * SCREEN_WIDTH, y: SCREEN_HEIGHT * 0.667119565217391, width: 12 * SCREEN_SCALE, height: 12 * SCREEN_SCALE))
         
@@ -69,6 +71,14 @@ class RigisterWithDetailVC: UIViewController {
         d.image = #imageLiteral(resourceName: "select")
         return d
     }()
+    
+    // MARK: - 立即注册
+    lazy var rigistBtn: UIButton = {
+        let d : UIButton = UIButton.init(frame: CGRect.init(x: SCREEN_WIDTH * 0.0901771213697351, y: SCREEN_HEIGHT * 0.746829696323561, width: SCREEN_WIDTH * 0.8, height: 30 * SCREEN_SCALE))
+        d.layer.borderWidth = 1
+        return d
+    }()
+
     
     
     override func viewDidLoad() {
@@ -84,8 +94,9 @@ class RigisterWithDetailVC: UIViewController {
         view.addSubview(userAgree)
         view.addSubview(agreeeBtn)
         view.addSubview(agreeeImg)
+        view.addSubview(rigistBtn)
     }
-
+    
     //// 协议事件
     @objc fileprivate func agreeSEL(sender :UIButton) {
         sender.isSelected = !sender.isSelected
@@ -102,12 +113,12 @@ class RigisterWithDetailVC: UIViewController {
         self.navigationController?.popToRootViewController(animated: true)
     }
     
-    //    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    //        let touch: UITouch? = touches.first
-    //        let touchPoint: CGPoint? = touch?.location(in: self.view)
-    //        print("\((touchPoint?.x)! / SCREEN_WIDTH)==\((touchPoint?.y)! / SCREEN_HEIGHT)")
-    //        let stringFloat = Int((touchPoint?.x)!)
-    //        let stringFloat1 = Int((touchPoint?.y)!)
-    //        print("\(stringFloat)\(stringFloat1)")
-    //    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch: UITouch? = touches.first
+        let touchPoint: CGPoint? = touch?.location(in: self.view)
+        print("\((touchPoint?.x)! / SCREEN_WIDTH)==\((touchPoint?.y)! / SCREEN_HEIGHT)")
+        let stringFloat = Int((touchPoint?.x)!)
+        let stringFloat1 = Int((touchPoint?.y)!)
+        print("\(stringFloat)\(stringFloat1)")
+    }
 }
